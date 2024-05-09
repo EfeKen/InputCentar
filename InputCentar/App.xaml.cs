315c3d67b083f9843d16ceb2172e3ee1b6f84d25
poko7o <kenan.efendicc@gmail.com>
2024-05-09 11:02:44 +0200

    Add project files.

---
+++ InputCentar/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace InputCentar
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ InputCentar/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace InputCentar
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}

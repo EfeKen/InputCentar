7efdc191a43732785aa921003e55e4446712c3c4
STEM8 <STEM8@DESKTOP-N0MNTGF>
2024-05-16 12:28:42 +0200

    PushMyCar

--- InputCentar/Projekti.xaml.cs
+++ InputCentar/Projekti.xaml.cs
@@ -8,26 +8,57 @@ namespace InputCentar
         public Projekti()
         {
             InitializeComponent();
+            
+           
 
-            // Mock data for projects
+           
+
+          
+            
             var projects = new[]
             {
                 new { Name = "Project 1" },
                 new { Name = "Project 2" },
                 new { Name = "Project 3" },
-                // Add more projects as needed
+                new { Name = "Project 1" },
+                new { Name = "Project 2" },
+                new { Name = "Project 3" },
+                new { Name = "Project 2" },
+                new { Name = "Project 1" },
+                new { Name = "Project 2" },
+                new { Name = "Project 3" },
+                new { Name = "Project 1" },
+                new { Name = "Project 2" },
+                new { Name = "Project 1" },
+                new { Name = "Project 2" },
+                new { Name = "Project 1" },
+                new { Name = "Project 2" },
+                new { Name = "Project 3" },
+                new { Name = "Project 1" },
+                new { Name = "Project 2" },
+                new { Name = "Project 1" },
+                new { Name = "Project 2" },
+                new { Name = "Project 3" },
+                new { Name = "Project 3" },
+                new { Name = "Project 3" },
+                new { Name = "Project 3" },
+                new { Name = "Project 3" },
+
             };
 
-            // Set the items source of the ListView to the list of projects
+            
             ProjektiListView.ItemsSource = projects;
         }
 
-        private void AktivniProjekti_Clicked(object sender, EventArgs e)
+        private async void AktivniProjekti_Clicked(object sender, EventArgs e)
         {
-            // Handle button click here
-            // You can add code to navigate to another page or perform any action you want
-            // For example:
-            // Navigation.PushAsync(new AktivniProjektiPage());
+            await Navigation.PushAsync(new AktivniProjekti());
+        }
+        private async void BackButtonClicked(object sender, EventArgs e)
+        {
+            
+            await Navigation.PushAsync(new MainPage());
         }
     }
 }
+

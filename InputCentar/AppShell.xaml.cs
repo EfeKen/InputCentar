4279210384a7e4cd843643751914a3087dbf20d3
poko7o <kenan.efendicc@gmail.com>
2024-05-19 20:05:58 +0200

    Commit8

--- InputCentar/AppShell.xaml.cs
+++ InputCentar/AppShell.xaml.cs
@@ -6,19 +6,13 @@ namespace InputCentar
 {
     public partial class AppShell : Shell
     {
-        public ICommand NavigateToMainPageCommand { get; }
 
         public AppShell()
         {
             InitializeComponent();
 
-            // Initialize the command
-            NavigateToMainPageCommand = new Command(NavigateToMainPage);
         }
+    
 
-        private async void NavigateToMainPage()
-        {
-            await Shell.Current.GoToAsync("//MainPage");
-        }
     }
 }

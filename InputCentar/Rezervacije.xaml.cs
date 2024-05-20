d59db4e1a334425a82efbd33093e77dff1a6800c
STEM8 <STEM8@DESKTOP-N0MNTGF>
2024-05-20 13:50:09 +0200

    PushMyCar3

--- InputCentar/Rezervacije.xaml.cs
+++ InputCentar/Rezervacije.xaml.cs
@@ -14,11 +14,12 @@ namespace InputCentar
             // Initialize the collection of segment items
             SegmentItems = new ObservableCollection<SegmentItem>
             {
-                new SegmentItem { IconSource = "office.png", Title = "Kancelarije" },
-                new SegmentItem { IconSource = "creativity.png", Title = "Kreativnost" },
-                  new SegmentItem { IconSource = "cpu.png", Title = "Elektronika" },
-
-                  new SegmentItem { IconSource = "book.png", Title = "Materijali" },
+                new SegmentItem { IconSource = "office.png" },
+                new SegmentItem { IconSource = "creativity.png" },
+                  new SegmentItem { IconSource = "cpu.png"},
+                  new SegmentItem { IconSource = "book.png"},
+                new SegmentItem { IconSource = "it.png"},
+                new SegmentItem { IconSource = "stem.png" },
                 // Add more segment items as needed
             };
 
@@ -39,7 +40,106 @@ namespace InputCentar
                 }
             }
         }
+        private void IconTapped(object sender, EventArgs e)
+        {
+            // Handle the tap event here 
+            LoadItemList();
+        }
+        private void Icon1Tapped(object sender, EventArgs e)
+        {
+            // Handle the tap event here 
+            LoadItemList1();
+        }
+        private void Icon2Tapped(object sender, EventArgs e)
+        {
+            // Handle the tap event here 
+            LoadItemList2();
+        }
+        private void Icon3Tapped(object sender, EventArgs e)
+        {
+            // Handle the tap event here 
+            LoadItemList3();
+        }
+        private void Icon4Tapped(object sender, EventArgs e)
+        {
+            // Handle the tap event here 
+            LoadItemList4();
+        }
+        private void Icon5Tapped(object sender, EventArgs e)
+        {
+            // Handle the tap event here 
+            LoadItemList5();
+        }
+        private void LoadItemList()
+        {
+            Default.Children.Clear();
+
+           
+            ListView listView = new ListView();
+       
+            listView.ItemsSource = new List<string> { "Edukacija 1", "Edukacija 2", "Edukacija 3" };
+
+            Default.Children.Add(listView);
+        }
+        private void LoadItemList1()
+        {
+            Default.Children.Clear();
+
+
+            ListView listView = new ListView();
+
+            listView.ItemsSource = new List<string> { "Knjiga 1", "Knjiga 2", "Knjiga 3" };
+
+            Default.Children.Add(listView);
+        }
+        private void LoadItemList2()
+        {
+            Default.Children.Clear();
+
+
+            ListView listView = new ListView();
+
+            listView.ItemsSource = new List<string> { "Arduino 1", "Arduino 2", "Arduino 3" };
+
+            Default.Children.Add(listView);
+        }
+        private void LoadItemList3()
+        {
+            Default.Children.Clear();
+
+
+            ListView listView = new ListView();
+
+            listView.ItemsSource = new List<string> { "Kancelarija 1", "Kancelarija 2", "Kancelarija 3" };
+
+            Default.Children.Add(listView);
+        }
+        private void LoadItemList4()
+        {
+            Default.Children.Clear();
+
+
+            ListView listView = new ListView();
+
+            listView.ItemsSource = new List<string> { "Stem 1", "Stem 2", "Stem 3" };
+
+            Default.Children.Add(listView);
+        }
+        private void LoadItemList5()
+        {
+            Default.Children.Clear();
+
+
+            ListView listView = new ListView();
+
+            listView.ItemsSource = new List<string> { "Printer 1", "Printer 2", "Preinter 3" };
+
+            Default.Children.Add(listView);
+        }
     }
+
+   
+
 
     public class SegmentItem
     {

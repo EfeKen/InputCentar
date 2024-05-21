1d037c6795d113fabdb451c8a4b036845f3b5dfd
Belmin <belminskulj1@gmail.com>
2024-05-21 11:13:23 +0200

    Belmin

--- InputCentar/Edukacije.xaml.cs
+++ InputCentar/Edukacije.xaml.cs
@@ -6,4 +6,9 @@ public partial class Edukacije : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void ImageButton_Clicked(object sender, EventArgs e)
+    {
+        // Otvori URL u web pregledniku
+        await Launcher.OpenAsync(new Uri("https://www.facebook.com/CentarINPUT/"));
+    }
 }

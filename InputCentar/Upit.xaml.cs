d797e747911dc878b78df4c291adf74da992c2ed
Edvin Alicic <alicicedvin@gmail.com>
2024-06-04 12:49:47 +0200

    Izmjena upita

--- InputCentar/Upit.xaml.cs
+++ InputCentar/Upit.xaml.cs
@@ -7,8 +7,8 @@ public partial class Upit : ContentPage
 		InitializeComponent();
 	}
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
-
+        await Launcher.OpenAsync(new Uri("https://docs.google.com/forms/d/e/1FAIpQLSfyd90CTw-Z9Wlrt0wjcSn4Fc6WGrujCyE1SGwSR4wMQh7ufQ/viewform?usp=sf_link"));
     }
 }
